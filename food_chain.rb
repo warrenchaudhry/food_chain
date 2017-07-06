07cb50bd02446605ccc8c622c5f78bafb3679b83
Warren Chaudhry <warrenchaudhry@gmail.com>
2017-07-06 12:47:40 +0800

    create constants

---
+++ food_chain.rb
@@ -0,0 +1,17 @@
+class FoodChain
+
+  LINES = {
+    "fly" => "I don't know why she swallowed the fly. Perhaps she'll die.\n",
+    "spider" => "It wriggled and jiggled and tickled inside her.",
+    "bird" => "How absurd to swallow a bird!",
+    "cat" => "Imagine that, to swallow a cat!",
+    "dog" => "What a hog, to swallow a dog!",
+    "goat" => "Just opened her throat and swallowed a goat!",
+    "cow" => "I don't know how she swallowed a cow!",
+    "horse" => "She's dead, of course!\n"
+  }
+  ANIMALS = LINES.keys.freeze
+
+
+end
+
